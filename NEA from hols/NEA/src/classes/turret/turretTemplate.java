9b4bd38b65a3b3d6dd0f048d2c14398eeb87fc10
Jack Maguire <jackmaguire1234@gmail.com>
2020-01-12 16:42:47 +0000

    Update turretTemplate.java

--- NEA from hols/NEA/src/classes/turret/turretTemplate.java
+++ NEA from hols/NEA/src/classes/turret/turretTemplate.java
@@ -1,4 +1,69 @@
 package classes.turret;
 
 public class turretTemplate {
+
+    private String name;
+    private String info;
+    private String dmg;
+    private String fireRate;
+    private String range;
+
+    private int dmgInt;
+    private int fireRateInt;
+    private int rangeInt;
+    private String fn;
+    private String bullet_fn;
+
+    public turretTemplate(String name, String info, String dmg, String fireRate, String range, int dmgInt, int fireRateInt, int rangeInt, String fn, String bullet_fn) {
+        this.name = name;
+        this.info = info;
+        this.dmg = dmg;
+        this.fireRate = fireRate;
+        this.range = range;
+        this.dmgInt = dmgInt;
+        this.fireRateInt = fireRateInt;
+        this.rangeInt = rangeInt;
+        this.fn = fn;
+        this.bullet_fn = bullet_fn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getDmg() {
+        return dmg;
+    }
+
+    public String getFireRate() {
+        return fireRate;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public int getDmgInt() {
+        return dmgInt;
+    }
+
+    public int getFireRateInt() {
+        return fireRateInt;
+    }
+
+    public int getRangeInt() {
+        return rangeInt;
+    }
+
+    public String getFn() {
+        return fn;
+    }
+
+    public String getBullet_fn() {
+        return bullet_fn;
+    }
 }
