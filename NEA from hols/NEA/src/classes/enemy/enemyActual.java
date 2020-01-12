9af02ef1d21d63cafb3f524376a4f55322ce9917
Jack Maguire <jackmaguire1234@gmail.com>
2020-01-12 10:17:24 +0000

    added to enemy class
    added super, and removed old imageIO

--- NEA from hols/NEA/src/classes/enemy/enemyActual.java
+++ NEA from hols/NEA/src/classes/enemy/enemyActual.java
@@ -1,6 +1,8 @@
 package classes.enemy;
 
 import classes.Entity.Entity;
+import classes.Entity.entityType;
+import classes.util.Coordinate;
 import main.main;
 
 import javax.imageio.ImageIO;
@@ -14,29 +16,16 @@ public class enemyActual extends Entity {
     private enemyTemplate template;
 
     private int currentHP, currentSpd;
-    private BufferedImage img;
 
-    public enemyActual(enemyTemplate eTemplate) {
+    public enemyActual(enemyTemplate eTemplate, Coordinate XYInArr, String fn) {
+        super(XYInArr, fn, entityType.enemy);
+
         int offsetY = main.TILE_HEIGHT / 2;
         int offsetX = main.TILE_WIDTH / 2;
 
         template = eTemplate;
         currentHP = template.getHp();
         currentSpd = template.getSpd();
-
-        URL url;
-        img = new BufferedImage(main.TILE_WIDTH, main.TILE_HEIGHT, BufferedImage.TYPE_INT_RGB);
-
-        try {
-
-            url = new URL(main.ENEMY_IMAGES_LOC + eTemplate.getFn());
-            URLConnection con = url.openConnection();
-            Image temp = ImageIO.read(con.getInputStream()).getScaledInstance(main.TILE_WIDTH, main.TILE_HEIGHT, Image.SCALE_SMOOTH);
-            img.getGraphics().drawImage(temp, 0, 0, null);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }
