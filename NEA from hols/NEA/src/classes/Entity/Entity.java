4b3340e0af1b6ddbeac2fc1ce9adfe15dfda7bba
Jack Maguire <jackmaguire1234@gmail.com>
2020-01-12 10:16:33 +0000

    filled out entity class

--- NEA from hols/NEA/src/classes/Entity/Entity.java
+++ NEA from hols/NEA/src/classes/Entity/Entity.java
@@ -40,16 +40,54 @@ public abstract class Entity {
         } catch (IOException e) {
             temp = null;
         }
-        temp = temp.getScaledInstance()
+
+        Coordinate wh = getWHOnType(type); //wh == width and height
+        temp = temp.getScaledInstance(wh.getX(), wh.getY(), Image.SCALE_SMOOTH);
     }
 
-    private Coordinate getWHOnType (entityType type) {
+    //region getters and setters
+    public BufferedImage getImg() {
+        return img;
+    }
+
+    public Coordinate getXYInArr() {
+        return XYInArr;
+    }
+
+    public Coordinate getXYInTile() {
+        return XYInTile;
+    }
+
+    public Coordinate getXYOnScrn () {
+        int bigX = main.TILE_WIDTH * XYInArr.getX();
+        int bigY = main.TILE_HEIGHT * XYInArr.getY();
+
+        int smallX = XYInTile.getX();
+        int smallY = XYInTile.getY();
+
+        int x = bigX + smallX;
+        int y = bigY + smallY;
+
+        return new Coordinate(x, y);
+    }
+
+    public void changeTile (Coordinate newOne) {
+        XYInArr = newOne;
+    }
+
+    public void changePosInTile (Coordinate newOne) {
+        XYInTile = newOne;
+    }
+    //endregion
+
+    private static Coordinate getWHOnType (entityType type) {
         switch (type) {
             case enemy:
-                return new Coordinate(main.ENEMY_WIDTH, main.ENEMY_HEIGHT);
-                break;
+                return new Coordinate(main.ENEMY_WIDTH, main.ENEMY_HEIGHT); //No break statement needed as it becomes an unreachable statement
             case bullet:
                 return new Coordinate(main.BULLET_WIDTH, main.BULLET_HEIGHT);
+            default:
+                return new Coordinate(main.TURRET_WIDTH, main.TURRET_HEIGHT);
         }
     }
 }
